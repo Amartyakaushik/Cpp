87465e73ffc2e6efdade86430e5ce7d3e03d9f00
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-06-07 23:31:14 +0530

    Add files via upload

--- ClassWork/class01.cpp
+++ ClassWork/class01.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
+// ## INLINE FUNCTION
+// prevent function call overhead
+class Calculator{
+    int x;
+    int y;
+    public:
+    // This is an inline function as it's defined inside the class. 
+    void sum(int a, int b){
+        x = a;
+        y = b;
+        cout<< x+y;
+    }
 
+    void sub();
+};
+
+void Calculator::sub(int a, int b){
+    x =a;
+    y = b;
+    cout<< x - y;
+}
+
+int main(){
+    // int x;
+    // cout << "Enter the value of x";
+    // cin >> x;
+    // cout << "value of x is " << x;
+
+    Calculator c;
+    c.sum(3,4);
+    c.sub(4,5);
+
+    
+}
