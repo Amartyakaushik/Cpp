7bd8b0ea87effbf9e07a25538d39233b06e64e03
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-06-24 18:20:33 +0530

    Add files via upload

---
+++ ClassWork/Untitled24.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//class Complex{
+
+
+//	int real;
+//	char imaginary;
+//	public:
+//		Complex(int a, char b){
+//			real = a;
+//			imaginary = b;
+//		}
+//		
+//		Complex operator+(Complex obj){
+//			Complex temp;
+//			temp.real = real + obj.real;
+//			temp.imaginary = imaginary + obj.imaginary;
+//			return temp;
+//		}
+//		
+//		showData(){
+//			
+//		}
+//};
+
+int main(){
+	float a = float(3.0/2.0);
+	cout<<a;
+}
