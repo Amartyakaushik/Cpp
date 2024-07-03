819bb765f83a40f2596948765a2c213d8acd9f5b
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-07-03 23:31:30 +0530

    Add files via upload

---
+++ ClassWork/Error28th.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+#include<cmath> 
+#include<iomanip>
+using namespace std;
+int main(){
+	float r = 6.5;
+	float a = 3.14 * 6.5 *6.5;
+	float result = floor(a *100)/100;
+	cout<<fixed<<setprecision(2)<<a;
+//	cout<<
+}
