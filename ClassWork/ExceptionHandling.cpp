dd3a47cc6846feab6d9c002d6d027d1fa414ef8c
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-07-05 23:27:46 +0530

    Add files via upload

---
+++ ClassWork/ExceptionHandling.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int x,y;
+//	cin>>x>>y;
+//	
+//	int d ;
+//	try{
+//	if(y != 0){
+//		d = x / y;
+//		cout<<d;
+//	}else{
+//		throw (y);
+//	}
+//		
+//	}catch(int r){
+//		y = r;
+//		cout<< x/y;
+//	}
+
+	int arr[5];
+	arr[5]=22;
+	cout<<arr[5];
+}
