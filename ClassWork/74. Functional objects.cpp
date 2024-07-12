1c676222d7253cca1c903ebf9b20457b61d14ab1
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-07-12 22:20:03 +0530

    Add files via upload

---
+++ ClassWork/74. Functional objects.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<functional>
+#include<algorithm>
+using namespace std;
+
+//Function object -> function wrapped in a class so that it can be used like an object
+int main(){
+    int arr[]={234,3,64,2,275,7,44,5543,345};
+    // sort(arr,arr+7)   // arr+7 is used to sort till 7th index
+    sort(arr,arr+7,greater<int>());   // "greater<int>()" is a functional object used to sort array in descending order.
+    for(int i=0;i<9;i++){
+        cout<<arr[i]<<endl;
+    }
+    
+    
+}
